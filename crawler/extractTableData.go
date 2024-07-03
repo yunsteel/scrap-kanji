28eb14da22bc0b40b0a4c918623040249a738a1f
yunsteel <archer0082@naver.com>
2024-07-03 15:41:25 +0900

    되돌리기

--- crawler/extractTableData.go
+++ crawler/extractTableData.go
@@ -1,7 +1,6 @@
 package crawler
 
 import (
-	"slices"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -23,27 +22,14 @@ func ExtractText(n *html.Node) string {
 	return strings.TrimSpace(text)
 }
 
-func FilterRow(row *[]string) []string {
-	excludedHeader := []string{"구자", "삭제연도"}
-
-	res := []string{}
-
-	for _, rowItem := range *row {
-		if !slices.Contains(excludedHeader, rowItem) {
-			res = append(res, rowItem)
-		}
-	}
-
-	return res
-}
-
 func ExtractTableData(doc *html.Node) ([][]string, error) {
+	header := []string{"아이디", "한자", "구자", "부수", "총획", "학년", "추가연도", "삭제연도", "발음"}
+
 	var tableData [][]string
 	var extract func(*html.Node)
 
 	extract = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "table" {
-			var header = []string{"아이디", "한자", "구자", "부수", "총획", "학년", "추가연도", "삭제연도", "발음"}
 			var rows [][]string
 
 			for child := n.FirstChild; child != nil; child = child.NextSibling {
@@ -58,7 +44,7 @@ func ExtractTableData(doc *html.Node) ([][]string, error) {
 							}
 
 							if len(row) > 0 {
-								rows = append(rows, FilterRow(&row))
+								rows = append(rows, row)
 							}
 						}
 					}
@@ -66,7 +52,7 @@ func ExtractTableData(doc *html.Node) ([][]string, error) {
 				}
 			}
 
-			tableData = append(tableData, FilterRow(&header))
+			tableData = append(tableData, header)
 			tableData = append(tableData, rows...)
 		}
 
