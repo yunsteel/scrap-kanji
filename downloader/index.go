40920bbfa22bf2bea7a635b2a552c1face8f74b7
yunsteel <archer0082@naver.com>
2024-07-05 17:01:28 +0900

    파일 분리

---
+++ downloader/index.go
@@ -0,0 +1,71 @@
+package downloader
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"gocrawler/crawler"
+	"gocrawler/util"
+	"os"
+	"strconv"
+)
+
+func DownloadKanjiTable() {
+	kanjiList := crawler.CrawlWiki()
+
+	file, err := os.Open("./reference/kanji.csv")
+
+	if err != nil {
+		return
+	}
+
+	reader := csv.NewReader(bufio.NewReader(file))
+	rows, err := reader.ReadAll()
+
+	if err != nil {
+		return
+	}
+
+	defer file.Close()
+
+	table := [][]string{
+		{"아이디",
+			"한자",
+			"구자",
+			"부수",
+			"총획",
+			"학년",
+			"추가연도",
+			"삭제연도",
+			"발음",
+			"훈음",
+			"뜻",
+		},
+	}
+
+	kanjiMap := util.CsvToCollection(rows)
+
+	for _, kanjiItem := range kanjiList {
+		kanjiItem.HUN_UM = kanjiMap[kanjiItem.KANJI]
+
+		intValue, err := strconv.Atoi(kanjiItem.ID)
+
+		if err != nil && intValue >= 1699 {
+			kanjiItem.MEANING = crawler.CrawlWordMeaning(kanjiItem.KANJI)
+		}
+
+		row := []string{kanjiItem.ID,
+			kanjiItem.KANJI,
+			kanjiItem.OLD_KANJI,
+			kanjiItem.RADICAL, kanjiItem.STROKE,
+			kanjiItem.GRADE, kanjiItem.YEAR_ADDED,
+			kanjiItem.YEAR_DELETED, kanjiItem.PRONUNCIATION,
+			kanjiItem.HUN_UM,
+			kanjiItem.MEANING}
+
+		table = append(table, row)
+		fmt.Println("[데이터가 추가됐어요] ", row)
+	}
+
+	util.SaveToCSV(table, "./table.csv")
+}
--- main.go
+++ main.go
@@ -1,65 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"encoding/csv"
-	"fmt"
-	"gocrawler/crawler"
-	"gocrawler/util"
-	"os"
-)
+import "gocrawler/downloader"
 
 func main() {
-	kanjiList := crawler.CrawlWiki()
-
-	file, err := os.Open("./reference/kanji.csv")
-
-	if err != nil {
-		return
-	}
-
-	reader := csv.NewReader(bufio.NewReader(file))
-	rows, err := reader.ReadAll()
-
-	if err != nil {
-		return
-	}
-
-	defer file.Close()
-
-	table := [][]string{
-		{"아이디",
-			"한자",
-			"구자",
-			"부수",
-			"총획",
-			"학년",
-			"추가연도",
-			"삭제연도",
-			"발음",
-			"훈음",
-			"뜻",
-		},
-	}
-
-	kanjiMap := util.CsvToCollection(rows)
-
-	for _, kanjiItem := range kanjiList {
-		kanjiItem.HUN_UM = kanjiMap[kanjiItem.KANJI]
-		kanjiItem.MEANING = crawler.CrawlWordMeaning(kanjiItem.KANJI)
-
-		row := []string{kanjiItem.ID,
-			kanjiItem.KANJI,
-			kanjiItem.OLD_KANJI,
-			kanjiItem.RADICAL, kanjiItem.STROKE,
-			kanjiItem.GRADE, kanjiItem.YEAR_ADDED,
-			kanjiItem.YEAR_DELETED, kanjiItem.PRONUNCIATION,
-			kanjiItem.HUN_UM,
-			kanjiItem.MEANING}
-
-		table = append(table, row)
-		fmt.Println("[데이터가 추가됐어요] ", row)
-	}
-
-	util.SaveToCSV(table, "./table.csv")
+	downloader.DownloadKanjiTable()
 }
